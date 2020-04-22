00fad724cd7a7b8496bd38850c2eae9ae5e3085c
Zepeacedust <41028225+Zepeacedust@users.noreply.github.com>
2020-04-22 13:56:56 +0000

    fyrra batch af files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 20f;
+    public float horizontal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+    }
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * Time.fixedDeltaTime * speed);
+        transform.Rotate(Vector3.up, horizontal);
+    }
+}
