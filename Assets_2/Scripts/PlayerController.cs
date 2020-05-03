8b334a1be4497ecf839a4b20423ddd0bb2ccbd65
Zepeacedust <arnor0903@gmail.com>
2020-05-03 00:26:23 +0000

    oh shit

---
+++ Assets_2/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+    private float horizontalInput;
+    public GameObject projectile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Shoot"))
+        {
+            Instantiate(projectile, this.transform.position, Quaternion.identity);
+        }
+    }
+    private void FixedUpdate()
+    {
+        //keeep the player in bounds
+        if (transform.position.x < -10) { transform.position = new Vector3(-10, transform.position.y, transform.position.y); }
+        if (transform.position.x > 10) { transform.position = new Vector3( 10, transform.position.y, transform.position.y); }
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.fixedDeltaTime);
+    }
+}
