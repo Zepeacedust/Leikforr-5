f61339d4300123d2e19d4b1be06e2b2e24d4df43
Zepeacedust <arnor0903@gmail.com>
2020-05-03 00:30:25 +0000

    meira cleaning

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20f;
-    public float horizontal;
+    public float speed = 10;
+    private float horizontalInput;
+    public GameObject projectile;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +16,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Shoot"))
+        {
+            Instantiate(projectile, this.transform.position, Quaternion.identity);
+        }
     }
     private void FixedUpdate()
     {
-        transform.Translate(Vector3.forward * Time.fixedDeltaTime * speed);
-        transform.Rotate(Vector3.up, horizontal);
+        //keeep the player in bounds
+        if (transform.position.x < -10) { transform.position = new Vector3(-10, transform.position.y, transform.position.y); }
+        if (transform.position.x > 10) { transform.position = new Vector3( 10, transform.position.y, transform.position.y); }
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.fixedDeltaTime);
     }
 }
